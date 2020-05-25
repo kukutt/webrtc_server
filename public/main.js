461dedf9ddf972398af843b6ec9f0d1214035753
Glitch (hello-express) <none>
2020-05-25 18:00:22 +0000

    😺🏠 Checkpoint ./public/main.js:3115263/248

--- public/main.js
+++ public/main.js
@@ -1,7 +1,14 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
-console.log(Peer);
+let peer = new Peer();
+peer.on('open', (id) => {
+  console.log('My peer ID is: ' + id);
+});
+
+peer.on('error', (error) => {
+  console.error(error);
+});
 
 console.log("hello world :o");
 
