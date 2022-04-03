6fc22736576998bb637d95655a3485687412a88f
Glitch (peerjs-video) <none>
2022-04-03 08:07:33 +0000

    🚡🚣 Checkpoint ./public/main.js:43497581/9863

--- public/main.js
+++ public/main.js
@@ -1,7 +1,14 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
-let Peer = window.Peer;
+
+// Global vars
+var dataChannelLabel = "op";
+var reliableSocket = null;
+var peerConnection = null;
+var dataChannel = null;
+var remoteCandidates = [];
+var have_offer = false;
 
 let messagesEl = document.querySelector('.messages');
 let peerIdEl = document.querySelector('#connect-to-peer');
@@ -17,64 +24,171 @@ let renderVideo = (stream) => {
   videoEl.srcObject = stream;
 };
 
-// Register with the peer server
-let peer = new Peer({
-  host: '/',
-  path: '/peerjs/myapp'
-});
-peer.on('open', (id) => {
-  logMessage('My peer ID is: ' + id);
-});
-peer.on('error', (error) => {
-  console.error(error);
-});
 
-// Handle incoming data connection
-peer.on('connection', (conn) => {
-  logMessage('incoming peer connection!');
-  conn.on('data', (data) => {
-    logMessage(`received: ${data}`);
-  });
-  conn.on('open', () => {
-    conn.send('hello!');
-  });
-});
+    /**
+     * Add the various callback handlers to the PeerConnection.
+     * Shared between both clients.
+     */
+    var setupPeerConnection = function () {
+      
+      //peerConnection = new RTCPeerConnection();
+      
+      peerConnection = new RTCPeerConnection({
+        iceServers: [{
+            urls: "stun:208.91.197.54:3478"
+           }]
+     	});
+      
+      peerConnection.onaddstream = function(obj) {
+  		var vid = document.querySelector("video");
+  		vid.srcObject = obj.stream;
+      }
+      
+      peerConnection.onicecandidate = function (event) {
+        if (event.candidate) {
+          reliableSocket.sendMessage("candidate", event.candidate);
+        } else {
+          reliableSocket.sendMessage("candidate", {"candidate":""});
+          logMessage("All local candidates received");
+        }
+      };
 
-// Handle incoming voice/video connection
-peer.on('call', (call) => {
-  navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then((stream) => {
-      call.answer(stream); // Answer the call with an A/V stream.
-      call.on('stream', renderVideo);
-    })
-    .catch((err) => {
-      console.error('Failed to get local stream', err);
-    });
-});
+      peerConnection.ondatachannel = function (event) {
+        if (event.channel.label == dataChannelLabel) {
+          dataChannel = event.channel;
+          logMessage("DataChannel received");
+          setupDataChannel(event.channel);
+        } else {
+          logMessage("Unknown CataChannel label: " + event.channel.label);
+        }
+      }
+    };
+
+    /**
+     * Add the various callback handlers to the DataChannel.
+     * Shared between both clients.
+     */
+    var setupDataChannel = function (dataChannel) {
+      dataChannel.onopen = function (e) {
+        logMessage("DataChannel open and ready to be used");
+
+        $("#send_datachannel_msg").click(function () {
+          var msg = $("#datachannel_msg").val();
+          logMessage("Sending message: " + msg);
+          dataChannel.send(msg);
+        });
+      };
+
+      dataChannel.onclose = function () {
+        logMessage("DataChannel closed");
+      };
+
+      dataChannel.onerror = function (e) {
+        logMessage("DataChannel error: " + e.message);
+        console.log(e);
+      };
+
+      dataChannel.onmessage = function (e) {
+        logMessage("Received message: " + e.data);
+      };
+    };
+
+    var createAnswer = function (msg) {
+      setupPeerConnection();
+
+      var desc = new RTCSessionDescription(msg);
+
+      peerConnection.setRemoteDescription(desc)
+      .then(function () {
+        return peerConnection.createAnswer();
+      })
+      .then(function(answer) {
+        return peerConnection.setLocalDescription(answer);
+      })
+      .then(function() {
+        reliableSocket.sendMessage("answer", peerConnection.localDescription);
+      })
+      .catch(function () {
+        console.log("RTC Error");
+      });
+    };
+
+    var handleCandidate = function (msg) {
+      var candidate = new RTCIceCandidate(msg);
+      peerConnection.addIceCandidate(candidate).then(function () {
+        logMessage("New remote candidate received");
+      }).catch(function (e) {
+        console.log("Error: Failure during addIceCandidate()", e);
+      });
+    }
+
+
 
 // Initiate outgoing connection
 let connectToPeer = () => {
-  
-  
-  let peerId = peerIdEl.value;
-  logMessage(`Connecting to ${peerId}...`);
-  
-  let conn = peer.connect(peerId);
-  conn.on('data', (data) => {
-    logMessage(`received: ${data}`);
-  });
-  conn.on('open', () => {
-    conn.send('hi!');
-  });
-  
-  navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then((stream) => {
-      let call = peer.call(peerId, stream);
-      call.on('stream', renderVideo);
-    })
-    .catch((err) => {
-      logMessage('Failed to get local stream', err);
-    });
-};
+      var channel_name = $("#channel_name").val();
+      var wsAddress = "wss://openhisilicon.glitch.me/" + channel_name;
+      console.log("Attempting WebSocket connection to " + wsAddress);
+
+      reliableSocket = new WebSocket(wsAddress);
+
+      reliableSocket.onopen = function (event) {
+        // Socket is now ready to send and receive messages
+        console.log("reliableSocket is open and ready to use");
+        reliableSocket.sendMessage("hello", {});
+      };
+
+      reliableSocket.onerror = function (event) {
+        // Socket failed to connect
+      };
+
+      reliableSocket.onclose = function (event) {
+        console.log("ERROR: Reliable socket has closed");
+      };
+
+      // Simple helper to send JSON messages with a given type
+      reliableSocket.sendMessage = function (type, msg) {
+        logMessage("Sending msg of type: " + type);
+        var jsonStr = JSON.stringify(msg);
+        var msg_clone_ = JSON.parse(jsonStr);
+        msg_clone_["type"] = type;
+        reliableSocket.send(JSON.stringify(msg_clone_));
+      }
+
+      reliableSocket.onmessage = function (event) {
+        console.log("Got msg", event);
+        var msg = JSON.parse(event.data);
+
+        logMessage("Received msg of type: " + msg.type);
+        console.log(msg);
+
+        switch (msg.type) {
+          case "offer":
+            createAnswer(msg);
+            have_offer = true;
+            var i = 0;
+            for (i = 0; i < remoteCandidates.length; i++) {
+              handleCandidate(remoteCandidates[i]);
+            }
+            break;
+          case "candidate":
+            if (msg.candidate) {
+              if (!have_offer) {
+                remoteCandidates.push(msg);
+              } else {
+                handleCandidate(msg);
+              }
+            } else {
+              console.log("Remote peer has no more candidates");
+            }
+            break;
+          default:
+            console.log("WARNING: Ignoring unknown msg of type '" + msg.type + "'");
+            break;
+        }
+      };
+
+
+    };
 
 window.connectToPeer = connectToPeer;
