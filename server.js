9bcca464cb83d53aae5d9328c4cb0ea30cd1e6fa
Glitch (peerjs-video) <none>
2022-04-03 09:10:36 +0000

    📇📉 Checkpoint ./server.js:43497581/721

--- server.js
+++ server.js
@@ -1,6 +1,39 @@
+const express = require("express");
+
+const { ExpressPeerServer } = require("ws");
+
+const app = express();
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+// peerjs server
+const peerServer = ExpressPeerServer(listener, {
+  debug: true,
+  path: '/myapp'
+});
+
+app.use('/peerjs', peerServer);
+
+
+
+
+
+
 const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 443 })
+const wss = new WebSocket.Server({ port: 8080 })
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
