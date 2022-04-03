db77fb32cca5d4ba46e6673ca9f322bb949a1891
Glitch (peerjs-video) <none>
2022-04-03 07:44:31 +0000

    🎁👗 Checkpoint ./package.json:43497581/1300 ./server.js:43497581/1111 ./views/index.html:43497581/381

--- server.js
+++ server.js
@@ -1,32 +1,10 @@
-// server.js
-// where your node app starts
+const WebSocket = require('ws')
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
+const wss = new WebSocket.Server({ port: 8080 })
 
-const { ExpressPeerServer } = require("peer");
-
-const app = express();
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
-
-// peerjs server
-const peerServer = ExpressPeerServer(listener, {
-  debug: true,
-  path: "/myapp",
-});
-
-app.use("/peerjs", peerServer);
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('ho!')
+})
