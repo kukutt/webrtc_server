0c71d7147244c54cc7ab55d6369c1a4006ad9334
Glitch (peerjs-video) <none>
2022-04-06 02:20:20 +0000

    🏟🍄 Checkpoint ./server.js:43497581/704

--- server.js
+++ server.js
@@ -22,6 +22,20 @@ const listener = app.listen(process.env.PORT, () => {
 // Set up a headless websocket server that prints any
 // events that come in.
 const wsServer = new ws.Server({ noServer: true });
+
+
+// `server` is a vanilla Node.js HTTP server, so use
+// the same ws upgrade process described here:
+// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
+
+listener.on('upgrade', (request, conn, head) => {
+  wsServer.handleUpgrade(request, conn, head, conn => {
+    wsServer.emit('connection', conn, request);
+  });
+});
+
+
+
 wsServer.on('connection', (conn, request) => {
   
   const { pathname } = parse(request.url);
@@ -47,15 +61,7 @@ wsServer.on('connection', (conn, request) => {
   
 });
 
-// `server` is a vanilla Node.js HTTP server, so use
-// the same ws upgrade process described here:
-// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
 
-listener.on('upgrade', (request, conn, head) => {
-  wsServer.handleUpgrade(request, conn, head, conn => {
-    wsServer.emit('connection', conn, request);
-  });
-});
 
 
 wsServer.findSend = function(src, msg) {
